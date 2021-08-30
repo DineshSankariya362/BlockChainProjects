27659972b5a08adf1dc4227c0af349623ae94044
DineshSankariya632 <sankariyad@gmail.com>
2021-08-30 13:59:38 +0530

    new commit

---
+++ src/com/luv2code/dinesh/Controller.java
@@ -0,0 +1,13 @@
+package com.luv2code.dinesh;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+	
+	@RequestMapping("/")
+	public String home_page() {
+		return "home";
+	}
+	
+}
